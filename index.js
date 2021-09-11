eeac6fecb847dda781046e86351c674a1666f5b6
Ryan-Tierney <54774168+Ryan-Tierney@users.noreply.github.com>
2021-09-11 11:15:24 -0500

    adding countdown

--- index.js
+++ index.js
@@ -3,4 +3,12 @@ let time = startingMinutes * 60;
 
 const countdownTimer = document.getElementById("countdown");
 
-function updateCountdown() {}
+setInterval(updateCountdown, 1000);
+
+function updateCountdown() {
+  const minutes = Math.floor(time / 60);
+  let seconds = time % 60;
+
+  countdownTimer.innerHTML = `${minutes}: ${seconds}`;
+  time--;
+}
