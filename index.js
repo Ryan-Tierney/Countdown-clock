d658d0687b699f591afbb894ef75ae95280d5322
Ryan-Tierney <54774168+Ryan-Tierney@users.noreply.github.com>
2021-09-13 23:30:08 -0500

    added days variable

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-const startinghours = 2;
+const startingdays = 16;
 
-let time = startinghours * 60 * 60;
+let time = startingdays * 24 * 60 * 60;
 
 const countdownTimer = document.getElementById("countdown");
 
 setInterval(updateCountdown, 1000);
 
 function updateCountdown() {
-  const hours = Math.floor(time / 60 / 60);
-  const minutes = Math.floor(time / startinghours / 60);
+  const days = Math.floor(time / 24 / 60 / 60);
+  const hours = Math.floor(time / startingdays / 60 / 60);
+  const minutes = Math.floor(time / startingdays / 24 / 60);
   let seconds = time % 60;
 
   seconds = seconds < 10 ? "0" + seconds : seconds;
 
-  countdownTimer.innerHTML = `${hours}:${minutes}:${seconds}`;
+  countdownTimer.innerHTML = `Days ${days}  ${hours}:${minutes}:${seconds}`;
   time--;
 }
