40a9f79e22095f8dc16b49bd4a3f2d775cad7a27
Ryan-Tierney <54774168+Ryan-Tierney@users.noreply.github.com>
2021-09-13 23:21:51 -0500

    changed minutes variable

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ setInterval(updateCountdown, 1000);
 
 function updateCountdown() {
   const hours = Math.floor(time / 60 / 60);
-  const minutes = Math.floor(time / 60);
+  const minutes = Math.floor(time / startinghours / 60);
   let seconds = time % 60;
 
   seconds = seconds < 10 ? "0" + seconds : seconds;
