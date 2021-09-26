11219b8b35f5f3e95541ed6b0e6183b436e3e69f
Ryan-Tierney <Ryan.Tierney92@gmail.com>
2021-09-26 10:06:56 -0500

    changing functionality

--- index.js
+++ index.js
@@ -1,25 +1,5 @@
-const startingdays = 16;
+var countDownDate = new Date("September 28, 2021 22:00:00").getTime();
 
-let time = startingdays * 24 * 60 * 60;
+var myfunc = setInterval(function () {}, 1000);
 
-const countdownTimer = document.getElementById("countdown");
-
-const startTimerButton = document.getElementById("startButton");
-
-startTimerButton.onClick = () => {
-  updateCountdown();
-};
-
-setInterval(updateCountdown, 1000);
-
-function updateCountdown() {
-  const days = Math.floor(time / 24 / 60 / 60);
-  const hours = Math.floor(time / startingdays / 60 / 60);
-  const minutes = Math.floor(time / startingdays / 24 / 60);
-  let seconds = time % 60;
-
-  seconds = seconds < 10 ? "0" + seconds : seconds;
-
-  countdownTimer.innerHTML = `Days ${days}  ${hours}:${minutes}:${seconds}`;
-  time--;
-}
+var n;
