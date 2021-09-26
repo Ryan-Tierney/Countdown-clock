07b3792c288ccfc6a83211f40f860b4150f5459e
Ryan-Tierney <Ryan.Tierney92@gmail.com>
2021-09-26 10:17:29 -0500

    added new time variables

--- index.js
+++ index.js
@@ -2,4 +2,10 @@ var countDownDate = new Date("September 28, 2021 22:00:00").getTime();
 
 var myfunc = setInterval(function () {}, 1000);
 
-var n;
+var now = new Date().getTime();
+var timeleft = countDownDate - now; //
+
+var days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
+var hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60));
+var minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
+var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
