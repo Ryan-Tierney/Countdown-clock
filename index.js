b76cb5508f0134703a4f0398271675f420401e59
Ryan-Tierney <Ryan.Tierney92@gmail.com>
2021-09-26 10:36:40 -0500

    changing CSS

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 var countDownDate = new Date("September 28, 2021 22:00:00").getTime();
 
-var myfunc = setInterval(function () {}, 1000);
+var myfunc = setInterval(function () {
+  var now = new Date().getTime();
+  var timeleft = countDownDate - now; //
 
-var now = new Date().getTime();
-var timeleft = countDownDate - now; //
+  var days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60));
+  var minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
 
-var days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
-var hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60));
-var minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
-var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
+  document.getElementById("days").innerHTML = days + "d ";
+  document.getElementById("hours").innerHTML = hours + "h ";
+  document.getElementById("minutes").innerHTML = minutes + "m ";
+  document.getElementById("seconds").innerHTML = seconds + "s ";
+
+  if (timeleft < 0) {
+    clearInterval(myfunc);
+    document.getElementById("days").innerHTML = "";
+    document.getElementById("hours").innerHTML = "";
+    document.getElementById("minutes").innerHTML = "";
+    document.getElementById("seconds").innerHTML = "";
+    document.getElementById("end").innerHTML = "LET'S RACE!!!!!";
+  }
+}, 1000);
