f814a1469a886f12062d50a6f4f55eb19e582b64
Ryan-Tierney <Ryan.Tierney92@gmail.com>
2021-09-27 11:25:35 -0500

    adding styling

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ var myfunc = setInterval(function () {
   var timeleft = countDownDate - now; //
 
   var days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60));
+  var hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
 
-  document.getElementById("days").innerHTML = days + "d ";
-  document.getElementById("hours").innerHTML = hours + "h ";
-  document.getElementById("minutes").innerHTML = minutes + "m ";
-  document.getElementById("seconds").innerHTML = seconds + "s ";
+  document.getElementById("days").innerHTML = days + " Day  ";
+  document.getElementById("hours").innerHTML = hours + ":";
+  document.getElementById("minutes").innerHTML = minutes + ":";
+  document.getElementById("seconds").innerHTML = seconds;
 
   if (timeleft < 0) {
     clearInterval(myfunc);
